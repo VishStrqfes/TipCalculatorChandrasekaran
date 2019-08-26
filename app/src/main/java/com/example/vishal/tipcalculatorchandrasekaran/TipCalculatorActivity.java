2743106d4ed9924259ae684af0537a335f2fc2f1
cool <vishalc6h@gmail.com>
2019-08-26 00:02:31 -0500

    coolv2wor

--- app/src/main/java/com/example/vishal/tipcalculatorchandrasekaran/TipCalculatorActivity.java
+++ app/src/main/java/com/example/vishal/tipcalculatorchandrasekaran/TipCalculatorActivity.java
@@ -18,6 +18,7 @@ public class TipCalculatorActivity extends AppCompatActivity {
     private Button calculateButton;
     private String bill;
     private String tip;
+    String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
