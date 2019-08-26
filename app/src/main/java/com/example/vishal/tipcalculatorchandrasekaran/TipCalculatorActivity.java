d2461261d7efb28df072fdf26c12a688427a9e53
Vishal <vishalc6h@gmail.com>
2019-08-25 23:54:12 -0500

    cool

--- app/src/main/java/com/example/vishal/tipcalculatorchandrasekaran/TipCalculatorActivity.java
+++ app/src/main/java/com/example/vishal/tipcalculatorchandrasekaran/TipCalculatorActivity.java
@@ -18,6 +18,7 @@ public class TipCalculatorActivity extends AppCompatActivity {
     private Button calculateButton;
     private String bill;
     private String tip;
+    String z;
     DecimalFormat decimalFormat = new DecimalFormat("$##.00");
 
     @Override
